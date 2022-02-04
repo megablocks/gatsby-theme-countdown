4a87d02102edf9b1763f366474cdff6374db06cd
megablocks <64942237+megablocks@users.noreply.github.com>
2022-02-03 23:30:30 -0800

    Update gatsby-config.js

--- theme/gatsby-config.js
+++ theme/gatsby-config.js
@@ -1,15 +1,18 @@
+/**
+ * Configure your Gatsby site with this file.
+ *
+ * See: https://www.gatsbyjs.org/docs/gatsby-config/
+ */
+
 module.exports = {
   siteMetadata: {
-    title: "My Countdown",
-    author: "Rafael Quintanilha"
+    url: "https://flamboyant-kilby-43f6fe.netlify.app/"
   },
   plugins: [
     {
       resolve: "@rafaelquintanilha/gatsby-theme-countdown",
       options: {
-        basePath: "/countdown",
-        mainHeader: "My countdown will end in:",
-        targetDate: "2020-04-28 00:00:00",
+        basePath: "/",
       }
     }
   ],
